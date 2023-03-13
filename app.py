32b26a1c347e7f7cca51101d23cc62fd91d6994e
Adam Liu <adam0703ls@gmail.com>
2023-03-13 16:34:49 +0800

    MODIFY read from prefixed env var

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
 from datetime import datetime
 from flask import Flask
 from flask_migrate import Migrate, upgrade
-from models import db,Comic
+from models import db, Comic
 from comic_api import comic_api
 from sqlalchemy import insert
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
 
-#TODO: read cors list from env
+# TODO: read cors list from env
 CORS(app)
 
-#TODO: read database connection string from env
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite+pysqlite:///:memory:" 
-app.config["SQLALCHEMY_ECHO"] = "True"
-
-app.register_blueprint(comic_api,url_prefix="/comic")
+app.register_blueprint(comic_api, url_prefix="/comic")
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
 with app.app_context():
-    # !!! AUTO MIGRATE !!! Only enable this line in debug mode
-    upgrade(directory='migrations', revision='head')
+    if app.config.get("AUTOMIGRATE") == "True":
+        upgrade(directory='migrations', revision='head')
 
-    db.session.execute(
-        insert(Comic),
-        [
-            {"name":"The adventure of meow", "description":"cat's adventure", "publish_date":datetime.now()},
-            {"name":"The roar of meow", "description":"cat's roar", "publish_date":datetime.now()},
-            {"name":"The purr of meow", "description":"cat's purring", "publish_date":datetime.now()},
-            {"name":"The adventure of meow vol.2", "description":"cat's adventure 2", "publish_date":datetime.now()}
+    if app.config.get("SEED_DEV_DATA") == "True":
+        db.session.execute(
+            insert(Comic),
+            [
+                {"name": "The adventure of meow", "description": "cat's adventure",
+                    "publish_date": datetime.now()},
+                {"name": "The roar of meow", "description": "cat's roar",
+                    "publish_date": datetime.now()},
+                {"name": "The purr of meow", "description": "cat's purring",
+                    "publish_date": datetime.now()},
+                {"name": "The adventure of meow vol.2",
+                    "description": "cat's adventure 2", "publish_date": datetime.now()}
 
-        ]
-    )
-    db.session.commit()
-    db.session.close()
+            ]
+        )
+        db.session.commit()
+        db.session.close()
